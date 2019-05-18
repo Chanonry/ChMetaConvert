0018da45e312a1191003bc1a54e9080864f4d42b
Chanonry <andy.spence@me.com>
2019-05-18 18:38:29 +0100

    concurrent pattern

--- docToItemConvert.go
+++ docToItemConvert.go
@@ -1,31 +1,43 @@
 // Package ChMetaConvert : converts the CHMetaTest table document format to a new List Item format table
-// utility progam
 package ChMetaConvert
 
 import (
-	// "context"
-	// "fmt"
-
-	// import command syntax - go get github.com/Chanonry/dbhelpers
 	"context"
+	"errors"
 	"fmt"
-	"time"
+	"strconv"
+	"sync"
 
 	DbHelpers "github.com/Chanonry/dbhelpers"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbattribute"
-	"github.com/davecgh/go-spew/spew"
+	"github.com/cenkalti/backoff"
 )
 
-const version = "1.0.0" //synchronous
+const version = "1.2.0" //concurrent
 
+// synchronous
 // performance:
 // total 6.255s for 1 company 23 document records
 // UnMarshal 213 ms
 // Marshal 22 µs
 // PutItem 200ms x 23
 // my internet
+
+// concurrent
+// paginate table - 23 ms per item or 8ms concurrently
+// with 4 goroutines - marshalling tales about 590µs per accounts item
+
+const testPages int = 2     // testing parameter
+const scanners int64 = 2    // number of table scan goroutines
+const marshallers int64 = 4 // number of marshaller goroutines
+const batchSize int = 25    // batch size for BatchWrite
+
+var scanner int64
+var scannerWG sync.WaitGroup
+var marshaller int64
+var marshallerWG sync.WaitGroup
 
 var dbsvc, _ = DbHelpers.NewService()
 
@@ -58,167 +70,351 @@ type cHMetaItem struct {
 	accounts []docMetaData
 }
 
-// extract one item from target table and unmarshal into cHMetaItem struct
-func unMarshallItem(tab *DbHelpers.DynamoTable) cHMetaItem {
+func paginateTablePool(tb *DbHelpers.DynamoTable, c chan map[string]dynamodb.AttributeValue) {
 
-	start := time.Now()
+	// profiling:
+	// 1081 Items
+	// 3 pages
+	// 128.5 Capacity units
+	// approx total is 2000 capacity units
+	// 13 second duration
+	// 12 ms per item so much faster I can put items
+	// on single pages was taking ~ 20ms per item
 
-	res, err := tab.GetItem("00363849", "")
-	if err != nil {
-		fmt.Println("GetItem error:", err)
+	// concurrently does it in about 8ms 1426 items in 11.62secs
+
+	// batch write items of 25 items, 400kb each total 16Mb
+	for scanner = 0; scanner < scanners; scanner++ {
+		scannerWG.Add(1)
+		go func(sc, scs int64) {
+
+			gor := strconv.FormatInt(sc, 10) // str repr of goroutine number
+			fmt.Println("Launching Paginator GOR:", gor)
+
+			// initialise variables
+			startKey := map[string]dynamodb.AttributeValue{}
+			pages := 0
+			total := 0
+			var totalConsumedCapacity float64
+			totalConsumedCapacity = 0.0
+
+			for {
+				input := &dynamodb.ScanInput{
+					TableName:              aws.String(tb.TableName),
+					ReturnConsumedCapacity: dynamodb.ReturnConsumedCapacityTotal,
+				}
+
+				if len(startKey) != 0 { // no startKey for first loop but add for later pages
+					input.ExclusiveStartKey = startKey
+				}
+
+				if scs > 1 { // multiple scanning goroutines demanded
+					input.Segment = &sc
+					input.TotalSegments = &scs
+				}
+
+				req := tb.Svc.ScanRequest(input)
+				// fmt.Println("Sending request GOR:", gor)
+				result, err := req.Send(context.Background())
+				if err != nil {
+					fmt.Printf("Paginate GOR%v: failed to make Query API call, %v\n", gor, err)
+				}
+
+				// put items on the channel
+				// fmt.Println("Putting Result on channel GOR:", gor)
+				for _, item := range result.Items {
+					c <- item
+					total++
+				}
+
+				if len(result.LastEvaluatedKey) != 0 {
+					startKey = result.LastEvaluatedKey
+					// fmt.Printf("GOR%v: LastEvaluatedKey%v\n", gor, result.LastEvaluatedKey)
+					// fmt.Printf("GOR%v: startKey%v\n", gor, startKey)
+					pages++
+					fmt.Printf("Paginate GOR:%v Page:%v\n", gor, pages)
+					fmt.Printf("Paginate GOR:%v Page:%v TestPages:%v\n", gor, pages, testPages)
+					totalConsumedCapacity += *result.ConsumedCapacity.CapacityUnits
+				}
+
+				if len(result.LastEvaluatedKey) == 0 || pages == testPages { // no more pages OR test finished
+					fmt.Printf("Paginate GOR%v: Processed items count:%v\n", gor, total)
+					fmt.Printf("Paginate GOR%v: Processed pages count:%v\n", gor, pages)
+					fmt.Printf("Paginate GOR%v: Total Consumed Capacity:%v\n", gor, totalConsumedCapacity)
+					fmt.Println("Stopping Paginate GOR:", gor)
+
+					scannerWG.Done()
+
+					return
+				}
+			}
+		}(scanner, scanners)
 	}
+}
 
-	var coMeta = cHMetaItem{} //initialise the target struct for Unmarshaling
-	//range across the top 3 fields - Name, Number and Accounts
-	for k, v := range res.Item { // just the Item data no metadata or headers
-		switch k {
-		case "Name":
-			{
-				var coName string
-				nameAttrVal := v
-				_ = dynamodbattribute.Unmarshal(&nameAttrVal, &coName)
-				coMeta.name = coName
-			}
-		case "Number":
-			{
-				var coNum string
-				numAttrVal := v
-				_ = dynamodbattribute.Unmarshal(&numAttrVal, &coNum)
-				coMeta.number = coNum
-			}
-		case "Accounts":
-			{
-				tempDocMeta := docMetaData{}
-				for _, allDocs := range v.M { // extract the map for all accounts docs
-					for k, v := range allDocs.M { // exptract the map for one accounts doc
-						attrPointer := v
-						temp := ""
-						err := dynamodbattribute.Unmarshal(&attrPointer, &temp)
-						if err != nil {
-							fmt.Println("Marshalling error:", err)
+func (tb *table) unMarshallPool(ipCh chan map[string]dynamodb.AttributeValue, opCh chan map[string]dynamodb.AttributeValue) {
+
+	// processing 1 item in ~ 590µs !
+	// 19693 items in 11.649s
+	for marshaller = 0; marshaller < marshallers; marshaller++ {
+		marshallerWG.Add(1)
+		go func(ms int64) {
+			gor := strconv.FormatInt(ms, 10) // str repr of goroutine number
+			fmt.Println("Launching Marshall GOR:", gor)
+
+			for item := range ipCh {
+				// fmt.Println("Taking Item of ipCh Marshall GOR:", gor)
+				for k, v := range item { // just the Item data no metadata or headers
+					var coMeta = cHMetaItem{} //initialise the target struct for Unmarshaling
+
+					switch k {
+					case "Name":
+						{
+							var coName string
+							nameAttrVal := v
+							_ = dynamodbattribute.Unmarshal(&nameAttrVal, &coName)
+							coMeta.name = coName
 						}
+					case "Number":
+						{
+							var coNum string
+							numAttrVal := v
+							_ = dynamodbattribute.Unmarshal(&numAttrVal, &coNum)
+							coMeta.number = coNum
+						}
+					case "Accounts":
+						{
+							tempDocMeta := docMetaData{}
+							for _, allDocs := range v.M { // extract the map for all accounts docs
+								for k, v := range allDocs.M { // exptract the map for one accounts doc
+									attrPointer := v
+									temp := ""
+									err := dynamodbattribute.Unmarshal(&attrPointer, &temp)
+									if err != nil {
+										fmt.Println("Marshalling error:", err)
+									}
 
-						switch k {
-						case "DocURL":
-							{
-								tempDocMeta.docURL = temp
-							}
-						case "ContentFile":
-							{
-								tempDocMeta.contentFile = temp
-							}
-						case "MadeUpDate":
-							{
-								tempDocMeta.madeUpDate = temp
-							}
-						case "Description":
-							{
-								tempDocMeta.description = temp
+									switch k {
+									case "DocURL":
+										{
+											tempDocMeta.docURL = temp
+										}
+									case "ContentFile":
+										{
+											tempDocMeta.contentFile = temp
+										}
+									case "MadeUpDate":
+										{
+											tempDocMeta.madeUpDate = temp
+										}
+									case "Description":
+										{
+											tempDocMeta.description = temp
+										}
+									}
+								}
+								coMeta.accounts = append(coMeta.accounts, tempDocMeta)
 							}
 						}
 					}
-					coMeta.accounts = append(coMeta.accounts, tempDocMeta)
+					for _, acct := range coMeta.accounts { // marshall into item with scalar attributes
+
+						attrValMap := make(map[string]dynamodb.AttributeValue)
+
+						valNUM, err := dynamodbattribute.Marshal(coMeta.number)
+						if err != nil {
+							fmt.Println("error marshalling")
+						}
+						attrValMap[tb.PrimaryKey] = *valNUM
+
+						valMUD, err := dynamodbattribute.Marshal(acct.madeUpDate)
+						if err != nil {
+							fmt.Println("error marshalling")
+						}
+						attrValMap[tb.SearchKey] = *valMUD
+
+						valNAME, err := dynamodbattribute.Marshal(coMeta.name)
+						if err != nil {
+							fmt.Println("error marshalling")
+						}
+						attrValMap["name"] = *valNAME
+
+						valFILE, err := dynamodbattribute.Marshal(acct.contentFile)
+						if err != nil {
+							fmt.Println("error marshalling")
+						}
+						attrValMap["contentFile"] = *valFILE
+
+						valDESC, err := dynamodbattribute.Marshal(acct.description)
+						if err != nil {
+							fmt.Println("error marshalling")
+						}
+						attrValMap["description"] = *valDESC
+
+						valURL, err := dynamodbattribute.Marshal(acct.docURL)
+						if err != nil {
+							fmt.Println("error marshalling")
+						}
+						attrValMap["docURL"] = *valURL
+
+						// fmt.Println("Putting map onto chan Marshall GOR:", gor)
+						opCh <- attrValMap
+					}
 				}
 			}
-		}
+			fmt.Println("Marshall goroutine finished :", gor)
+			marshallerWG.Done()
+		}(marshaller)
 	}
-	t1 := time.Now()
-	fmt.Println("UnMarshal duration:", t1.Sub(start))
-	return coMeta
+	return
 }
 
-// marshall a cHMetaItem struct into a []map[string][string]
-func marchallAttrs(st *cHMetaItem) []map[string]string {
-	start := time.Now()
-	xItems := []map[string]string{}
+// batchWritePool : creates a pool of workers to perform BatchWrites to dynamodb table
+func (tb *table) batchWritePool(ipCh chan map[string]dynamodb.AttributeValue) {
 
-	for _, acct := range st.accounts {
-		tempMap := map[string]string{}
+	for marshaller = 0; marshaller < marshallers; marshaller++ {
+		marshallerWG.Add(1)
 
-		tempMap["name"] = st.name
-		tempMap["number"] = st.number
-		tempMap["contentFile"] = acct.contentFile
-		tempMap["description"] = acct.description
-		tempMap["docUrl"] = acct.docURL
-		tempMap["madeUpDate"] = acct.madeUpDate
+		go func(ms int64) {
+			writeRequestS := []dynamodb.WriteRequest{}
+			count := 0
 
-		xItems = append(xItems, tempMap)
+			for attrValMap := range ipCh {
+				count++
+				writeRequest := dynamodb.WriteRequest{ // build the write request input
+					PutRequest: &dynamodb.PutRequest{ // one item
+						Item: attrValMap,
+					},
+				}
+				writeRequestS = append(writeRequestS, writeRequest) // append to the xs
 
+				if count == batchSize { // write the batch
+					BatchInput := dynamodb.BatchWriteItemInput{
+						RequestItems: map[string][]dynamodb.WriteRequest{
+							tb.TableName: writeRequestS},
+					}
+					tb.batchWriteRetry(BatchInput)
+					writeRequestS = []dynamodb.WriteRequest{}
+
+				}
+			} // range on channel loop
+
+			// channel closed - flush any remaining PutRequests
+			BatchInput := dynamodb.BatchWriteItemInput{
+				RequestItems: map[string][]dynamodb.WriteRequest{
+					tb.TableName: writeRequestS},
+			}
+			tb.batchWriteRetry(BatchInput)
+
+			marshallerWG.Done()
+		}(marshaller)
 	}
-	t1 := time.Now()
-	fmt.Println("marshal duration:", t1.Sub(start))
-	return xItems
 }
 
-// PutAttrsItem : create a new item with scalar attributes
-func (tb *table) PutAttrsItem(a map[string]string) (*dynamodb.PutItemOutput, error) {
-	start := time.Now()
+// batchWriteRetry : retry logic wrapping the batchWriter
+func (tb *table) batchWriteRetry(bi dynamodb.BatchWriteItemInput) {
 
-	// errorReturnValue := dynamodb.PutItemOutput{}
-
-	//initialise the map of attrVals for input
-	attrValMap := make(map[string]dynamodb.AttributeValue)
-
-	// add the data Attributes to input data
-	for k, v := range a { //range over the map to get each attribute
-		val, err := dynamodbattribute.Marshal(v)
-		if err != nil {
-			fmt.Println("error marshalling")
-			// return &errorReturnValue, err
-		}
-
-		switch k {
-		case "number":
-			{
-				attrValMap[tb.PrimaryKey] = *val
-			}
-		case "madeUpDate":
-			{
-				attrValMap[tb.SearchKey] = *val
-			}
-		default:
-			{
-				attrValMap[k] = *val
-			}
-		}
+	operation := func() error { //trying to get into the correct type for backoff
+		return batchWriter(tb, bi)
 	}
 
-	fmt.Println("input Attrs:", attrValMap)
-
-	ItemInput := &dynamodb.PutItemInput{
-		TableName:              aws.String(tb.TableName),
-		Item:                   attrValMap,
-		ReturnConsumedCapacity: dynamodb.ReturnConsumedCapacityTotal,
+	err := backoff.Retry(operation, backoff.NewExponentialBackOff())
+	if err != nil { // still errors after backoff then stop
+		fmt.Println("writer failed, goroutine terminated ", err)
+		return
 	}
+	return
+}
 
-	req := tb.Svc.PutItemRequest(ItemInput)
+// batchWriter : write batches of items to Dynamo Table
+func batchWriter(tb *table, bi dynamodb.BatchWriteItemInput) error {
+	req := tb.Svc.BatchWriteItemRequest(&bi)
 	result, err := req.Send(context.Background())
-	if err != nil {
-		// dynamodbErrorhandler(err)
-		fmt.Println("PutItem response error:", err)
+	if len(result.UnprocessedItems) != 0 {
+		err = errors.New("Unprocessed Items") // as well as dynamodb errors, force Retry
 	}
-	t1 := time.Now()
-	fmt.Println("PutItem duration:", t1.Sub(start))
-
-	return result, err
+	return err
 }
 
 func main() {
-	start := time.Now()
-	coStruct := unMarshallItem(&chMetaTable)
-	// spew.Dump(coStruct)
 
-	xI := marchallAttrs(&coStruct)
-	spew.Dump(xI)
-
-	for _, item := range xI {
-		res, err := acctsMetaData.PutAttrsItem(item)
-		if err != nil {
-			fmt.Println("Put Item error")
-		}
-		fmt.Println(res)
-	}
-	t1 := time.Now()
-	fmt.Println("total duration:", t1.Sub(start))
+	itemCH := make(chan map[string]dynamodb.AttributeValue)
+	paginateTablePool(&chMetaTable, itemCH)
 
 }
+
+// marshall a cHMetaItem struct into a []map[string][string]
+// func marchallAttrs(st *cHMetaItem) []map[string]string {
+// 	start := time.Now()
+// 	xItems := []map[string]string{}
+//
+// 	for _, acct := range st.accounts {
+// 		tempMap := map[string]string{}
+//
+// 		tempMap["name"] = st.name
+// 		tempMap["number"] = st.number
+// 		tempMap["contentFile"] = acct.contentFile
+// 		tempMap["description"] = acct.description
+// 		tempMap["docUrl"] = acct.docURL
+// 		tempMap["madeUpDate"] = acct.madeUpDate
+//
+// 		xItems = append(xItems, tempMap)
+//
+// 	}
+// 	t1 := time.Now()
+// 	fmt.Println("marshal duration:", t1.Sub(start))
+// 	return xItems
+// }
+
+// // PutAttrsItem : create a new item with scalar attributes
+// func (tb *table) PutAttrsItem(a map[string]string) (*dynamodb.PutItemOutput, error) {
+// 	start := time.Now()
+//
+// 	// errorReturnValue := dynamodb.PutItemOutput{}
+//
+// 	//initialise the map of attrVals for input
+// 	attrValMap := make(map[string]dynamodb.AttributeValue)
+//
+// 	// add the data Attributes to input data
+// 	for k, v := range a { //range over the map to get each attribute
+// 		val, err := dynamodbattribute.Marshal(v)
+// 		if err != nil {
+// 			fmt.Println("error marshalling")
+// 			// return &errorReturnValue, err
+// 		}
+//
+// 		switch k {
+// 		case "number":
+// 			{
+// 				attrValMap[tb.PrimaryKey] = *val
+// 			}
+// 		case "madeUpDate":
+// 			{
+// 				attrValMap[tb.SearchKey] = *val
+// 			}
+// 		default:
+// 			{
+// 				attrValMap[k] = *val
+// 			}
+// 		}
+// 	}
+//
+// 	fmt.Println("input Attrs:", attrValMap)
+//
+// 	ItemInput := &dynamodb.PutItemInput{
+// 		TableName:              aws.String(tb.TableName),
+// 		Item:                   attrValMap,
+// 		ReturnConsumedCapacity: dynamodb.ReturnConsumedCapacityTotal,
+// 	}
+//
+// 	req := tb.Svc.PutItemRequest(ItemInput)
+// 	result, err := req.Send(context.Background())
+// 	if err != nil {
+// 		// dynamodbErrorhandler(err)
+// 		fmt.Println("PutItem response error:", err)
+// 	}
+// 	t1 := time.Now()
+// 	fmt.Println("PutItem duration:", t1.Sub(start))
+//
+// 	return result, err
+// }
--- docToItemConvert_test.go
+++ docToItemConvert_test.go
@@ -1,22 +1,48 @@
 package ChMetaConvert
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
 
-func TestUnMarshallItem(t *testing.T) {
-	// testing against existing db service
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
 
-	// problem here is that if this record gets updated then the test will fail due to too many accounts records
-	res := unMarshallItem(&chMetaTable)
-	if res.name != "GRAYDON UK LIMITED" {
-		t.Error("UnMarshal error - wrong name:", res.name)
-	}
-	if res.number != "00363849" {
-		t.Error("UnMarshal error - wrong number:", res.number)
-	}
-	if len(res.accounts) != 23 {
-		t.Error("UnMarshal error - wrong number of accounts strucrs:", len(res.accounts))
-	}
+// not proper isolated testing just functional tests for app
 
+func TestPaginateTable(t *testing.T) {
+
+	start := time.Now()
+	testCH := make(chan map[string]dynamodb.AttributeValue)
+	testMarshallCH := make(chan map[string]dynamodb.AttributeValue)
+	total := 0
+
+	paginateTablePool(&chMetaTable, testCH) //launches goroutines
+	acctsMetaData.unMarshallPool(testCH, testMarshallCH)
+
+	go func() {
+		for _ = range testMarshallCH {
+			total++
+		}
+	}()
+
+	scannerWG.Wait()
+	fmt.Println("closing testCH")
+	close(testCH)
+	marshallerWG.Wait()
+	fmt.Println("closing testMarshallCH")
+	close(testMarshallCH)
+
+	t1 := time.Now()
+	fmt.Println("Duration:", t1.Sub(start).String())
+	fmt.Println("Total:", total)
+	// if testPages == 1 && total != 353 {
+	// 	t.Error("Incorrect total items: Expected 353 Got ", total)
+	// } else {
+	// 	if testPages != 1 {
+	// 		t.Error("Testpages != 1, invalid results of test. Testpages: ", testPages)
+	// 	}
+	// }
 }
 
 func TestMain(t *testing.T) {
